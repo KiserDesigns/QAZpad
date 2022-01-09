cb4b3c0bef41daf0dbab474f16266b349dc72642
Noah Kiser <96897936+KiserDesigns@users.noreply.github.com>
2022-01-09 12:24:02 -0500

    Moved encoder definition to qazpad.c

--- QMK+Vial Firmware/qazpad/qazpad.c
+++ QMK+Vial Firmware/qazpad/qazpad.c
@@ -4,6 +4,19 @@ void matrix_init_kb(void) {
 	matrix_init_user();
 }
 
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    return encoder_update_user(index, clockwise);
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_DOWN);
+        } else {
+            tap_code(KC_UP);
+        }
+    }
+    return 1;
 }
